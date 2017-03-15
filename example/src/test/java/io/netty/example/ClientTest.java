8b42b8bcd0130461930edacaf2ee28d2d5c7cd78
brightchen <bright@datatorrent.com>
2017-03-15 16:20:26 -0700

    Change pom.xml for eclipse and test case for interact with java socket

---
+++ example/src/test/java/io/netty/example/ClientTest.java
@@ -0,0 +1,75 @@
+package io.netty.example;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.junit.Test;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.example.telnet.TelnetClientInitializer;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+
+public class ClientTest
+{
+
+  static final boolean SSL = false;  //System.getProperty("ssl") != null;
+  static final String HOST = "127.0.0.1";  //System.getProperty("host", "127.0.0.1");
+  static final int PORT = 12345;  //Integer.parseInt(System.getProperty("port", SSL? "8992" : "8023"));
+
+  @Test
+  public void test() throws Exception {
+      // Configure SSL.
+      final SslContext sslCtx;
+      if (SSL) {
+          sslCtx = SslContextBuilder.forClient()
+              .trustManager(InsecureTrustManagerFactory.INSTANCE).build();
+      } else {
+          sslCtx = null;
+      }
+
+      EventLoopGroup group = new NioEventLoopGroup();
+      try {
+          Bootstrap b = new Bootstrap();
+          b.group(group)
+           .channel(NioSocketChannel.class)
+           .handler(new TelnetClientInitializer(sslCtx));
+
+          // Start the connection attempt.
+          Channel ch = b.connect(HOST, PORT).sync().channel();
+
+          // Read commands from the stdin.
+          ChannelFuture lastWriteFuture = null;
+          BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+          for (;;) {
+              String line = in.readLine();
+              if (line == null) {
+                  break;
+              }
+
+              // Sends the received line to the server.
+              lastWriteFuture = ch.writeAndFlush(line + "\r\n");
+
+              // If user typed the 'bye' command, wait until the server closes
+              // the connection.
+              if ("bye".equals(line.toLowerCase())) {
+                  ch.closeFuture().sync();
+                  break;
+              }
+          }
+
+          // Wait until all messages are flushed before closing the channel.
+          if (lastWriteFuture != null) {
+              lastWriteFuture.sync();
+          }
+      } finally {
+          group.shutdownGracefully();
+      }
+  }
+}
---
+++ example/src/test/java/io/netty/example/InteractTest.java
@@ -0,0 +1,98 @@
+package io.netty.example;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.DefaultChannelPromise;
+import io.netty.channel.EventLoop;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.example.telnet.TelnetClientHandler;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * Test how Netty can handle socket which created outside
+ *
+ */
+
+public class InteractTest
+{
+  private static final int PORT_NUMBER = 12345;
+
+  private static final int BUFFER_SIZE = 256;
+
+  @Test
+  public void test() throws IOException, InterruptedException
+  {
+    ServerSocketChannel server = ServerSocketChannel.open();
+    server.socket().bind(new InetSocketAddress(PORT_NUMBER));
+    server.socket().setReuseAddress(true);
+    server.configureBlocking(false);
+
+    final Selector selector = Selector.open();
+    server.register(selector, SelectionKey.OP_ACCEPT);
+    
+    System.out.println("Listen on port: " + PORT_NUMBER);
+    
+    ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+    java.nio.channels.SocketChannel javaWorkChannel = null;
+    boolean accepted = false;
+    while (!accepted) {
+      int channelCount = selector.select();
+      if (channelCount > 0) {
+        Set<SelectionKey> keys = selector.selectedKeys();
+        Iterator<SelectionKey> iterator = keys.iterator();
+        while (iterator.hasNext()) {
+          SelectionKey key = iterator.next();
+          iterator.remove();
+
+          if (key.isAcceptable()) {
+            javaWorkChannel = server.accept();
+            javaWorkChannel.configureBlocking(false);
+            javaWorkChannel.register(selector, SelectionKey.OP_READ, javaWorkChannel.socket().getPort());
+            accepted = true;
+            break;
+          }
+        }
+      }
+    }
+   
+    NioSocketChannel nettyChannel = new NioSocketChannel(javaWorkChannel);
+    handleWorkSocket(nettyChannel);
+  }
+  
+  public void handleWorkSocket(final NioSocketChannel ch) throws InterruptedException
+  {
+    EventLoopGroup group = new NioEventLoopGroup();
+    try {
+      Bootstrap b = new Bootstrap();
+      b.group(group);
+      
+      ch.pipeline().addFirst(new TelnetClientHandler()).addFirst(new StringDecoder());
+      EventLoop eventLoop = group.next();
+      ch.unsafe().register(eventLoop, new DefaultChannelPromise(ch, eventLoop));
+
+      if(!ch.isRegistered()) {
+        System.err.println("Channel not registered yet.");
+        Assert.assertFalse(true);
+      }
+      
+      ch.closeFuture().sync();
+      
+    } finally {
+      group.shutdownGracefully();
+    }
+  }
+
+}
