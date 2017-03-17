c9cd275cd40017b2d3890a5d9e0046fb13a5c935
brightchen <bright@datatorrent.com>
2017-03-17 10:59:33 -0700

    add test case for switch work socket handle to netty

--- example/src/test/java/io/netty/example/InteractTest.java
+++ example/src/test/java/io/netty/example/InteractTest.java
@@ -12,6 +12,8 @@ import java.util.Set;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.google.common.collect.Sets;
+
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.DefaultChannelPromise;
@@ -33,10 +35,12 @@ public class InteractTest
 {
   private static final int PORT_NUMBER = 12345;
 
-  private static final int BUFFER_SIZE = 256;
-
+  /**
+   * test use netty to handle the work socket which create by java server socket.
+   * The netty will take over after accept
+   */
   @Test
-  public void test() throws IOException, InterruptedException
+  public void test1() throws IOException, InterruptedException
   {
     ServerSocketChannel server = ServerSocketChannel.open();
     server.socket().bind(new InetSocketAddress(PORT_NUMBER));
@@ -48,7 +52,6 @@ public class InteractTest
     
     System.out.println("Listen on port: " + PORT_NUMBER);
     
-    ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
     java.nio.channels.SocketChannel javaWorkChannel = null;
     boolean accepted = false;
     while (!accepted) {
@@ -76,6 +79,82 @@ public class InteractTest
   }
   
   
+  /**
+   * test use netty to handle the work socket which create by java server socket.
+   * - java server socket waiting for connection
+   * - client connected and a java work socket created
+   * - java work socket handle messages from client
+   * - netty take over. the java work socket should be suspended.
+   * 
+   */
+  @Test
+  public void test2() throws IOException, InterruptedException
+  {
+    ServerSocketChannel server = ServerSocketChannel.open();
+    server.socket().bind(new InetSocketAddress(PORT_NUMBER));
+    server.socket().setReuseAddress(true);
+    server.configureBlocking(false);
+
+    final Selector selector = Selector.open();
+    server.register(selector, SelectionKey.OP_ACCEPT);
+    
+    System.out.println("Listen on port: " + PORT_NUMBER);
+    
+    java.nio.channels.SocketChannel javaWorkChannel = null;
+    boolean accepted = false;
+    while (!accepted) {
+      int channelCount = selector.select();
+      if (channelCount > 0) {
+        Set<SelectionKey> keys = selector.selectedKeys();
+        Iterator<SelectionKey> iterator = keys.iterator();
+        while (iterator.hasNext()) {
+          SelectionKey key = iterator.next();
+          iterator.remove();
+
+          if (key.isAcceptable()) {
+            javaWorkChannel = server.accept();
+            javaWorkChannel.configureBlocking(false);
+            javaWorkChannel.register(selector, SelectionKey.OP_READ, javaWorkChannel.socket().getPort());
+            accepted = true;
+            break;
+          }
+        }
+      }
+    }
+   
+    //java work socket handle message
+    handleWorkSocketJavaChannel(selector, javaWorkChannel);
+    
+    NioSocketChannel nettyChannel = new NioSocketChannel(javaWorkChannel);
+    handleWorkSocket(nettyChannel);
+  }
+  
+  private void handleWorkSocketJavaChannel(Selector selector, java.nio.channels.SocketChannel javaWorkChannel) throws IOException
+  {
+    ByteBuffer buffer = ByteBuffer.allocate(2048);
+    boolean done = false;
+    while (!done) {
+      int readyChannels = selector.select();
+      if (readyChannels == 0)
+        continue;
+
+      Set<SelectionKey> selectedKeys = selector.selectedKeys();
+      Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
+
+      while (keyIterator.hasNext()) {
+
+        SelectionKey key = keyIterator.next();
+        if (key.isReadable()) {
+          javaWorkChannel.read(buffer);
+          System.out.println(buffer);
+          done = true;
+          break;
+        }
+        keyIterator.remove();
+      }
+    }
+  }
+
   private static class MyEchoHandler extends EchoServerHandler
   {
     @Override
